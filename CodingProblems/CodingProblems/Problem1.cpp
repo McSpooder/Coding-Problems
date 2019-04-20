18a81429f72f8aa54e7d5fa923ba3cbfa57ec152
Danny Wilde <hextra@live.com>
2019-04-20 23:43:47 +0100

    Added a class for problem 1 with a static method Run();

---
+++ CodingProblems/CodingProblems/Problem1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Problem1.h"
+#include <iostream>
+
+
+void Problem1::Run()
+{
+	std::cout << "finished executing";
+	int number;
+	std::cin >> number;
+}
+
+Problem1::Problem1()
+{
+}
+
+
+Problem1::~Problem1()
+{
+}
---
+++ CodingProblems/CodingProblems/Problem1.h
@@ -0,0 +1,11 @@
+#pragma once
+//This is the class declaration for problem one of project euler.
+//In the problem we have to find all the factors of 3 and 5 from 0 to 1000 and sum them up.
+class Problem1
+{
+public:
+	static void Run();
+	Problem1();
+	~Problem1();
+};
+
