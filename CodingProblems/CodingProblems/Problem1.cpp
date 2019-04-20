8a7c0b995f164a8d8ccfaa6467e94b5ea33d8112
Danny Wilde <hextra@live.com>
2019-04-21 00:04:45 +0100

    Problem 1 Run method is finished and output the correct result.

--- CodingProblems/CodingProblems/Problem1.cpp
+++ CodingProblems/CodingProblems/Problem1.cpp
@@ -2,12 +2,23 @@
 #include "Problem1.h"
 #include <iostream>
 
-
+//This method sums up all the factors of 3 or 5 from 0 to 1000.
 void Problem1::Run()
 {
-	std::cout << "finished executing";
-	int number;
-	std::cin >> number;
+	std::cout << "=======================================================================================================================" << std::endl;
+	std::cout << "---------------------------------------------------- PROBLEM 1 --------------------------------------------------------" << std::endl;
+	std::cout << "Find the sum of all the factors of 3 or 5 from 0 to 1000." << std::endl;
+
+	int sum = 0;
+
+	for (int i = 0; i < 1000; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			sum += i;
+		}
+	}
+
+	std::cout << "Answer: " << sum << std::endl << std::endl;
+	std::getchar();
 }
 
 Problem1::Problem1()
